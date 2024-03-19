8d34487f88190ba7df00cb818ef6bcc1443cb69e
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-03-19 16:22:51 +0600

    next screen working from indivisual items

---
+++ app/src/main/java/com/axiagroups/recyclerview/SingleContact.java
@@ -0,0 +1,38 @@
+package com.axiagroups.recyclerview;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SingleContact extends AppCompatActivity {
+    private TextView nameTV, addressTV;
+    private String name, address;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_single_contact);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            name = extras.getString("name");
+            address = extras.getString("address");
+        }
+
+        nameTV = findViewById(R.id.card_nameTV);
+        nameTV.setText(name);
+        addressTV = findViewById(R.id.card_adressTV);
+        addressTV.setText(address);
+
+    }
+}
--- app/src/main/java/com/axiagroups/recyclerview/adapter/ContactAdapter.java
+++ app/src/main/java/com/axiagroups/recyclerview/adapter/ContactAdapter.java
@@ -1,6 +1,9 @@
 package com.axiagroups.recyclerview.adapter;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +12,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.axiagroups.recyclerview.MainActivity;
 import com.axiagroups.recyclerview.R;
+import com.axiagroups.recyclerview.SingleContact;
 import com.axiagroups.recyclerview.model.Contact;
 
 import java.util.List;
@@ -36,6 +41,18 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHode
         Contact contact = contactList.get(position);
         holder.nameTV.setText(contact.getName());
         holder.addressTV.setText(contact.getAddress());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context.getApplicationContext(), SingleContact.class);
+//                Intent i = new Intent(this.context, SingleContact.class);
+//                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                i.putExtra("name",contact.getName());
+                i.putExtra("address",contact.getAddress());
+                context.startActivity(i);
+            }
+        });
     }
 
     @Override
