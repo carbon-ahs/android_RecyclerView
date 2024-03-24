2c354224ac395d01fb0f6ce49581faefef41b5ad
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-03-24 12:53:23 +0600

    bottom nav+droewe complete

--- app/src/main/java/com/axiagroups/recyclerview/MainActivity.java
+++ app/src/main/java/com/axiagroups/recyclerview/MainActivity.java
@@ -1,15 +1,33 @@
 package com.axiagroups.recyclerview;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    BottomNavigationView bottomNavigationView;
+    FragmentManager fragmentManager;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +39,74 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 //        https://youtu.be/HIBO5lgsKU8?si=fXmy4M-6FeyxzmnK&t=596
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.nav_drawer_open, R.string.nav_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigation_drawer);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int itemId = menuItem.getItemId();
+                if (itemId == R.id.bottom_home) {
+                    openFragment(new HomeFragment());
+                    return true;
+                } else if (itemId == R.id.bottom_info) {
+                    openFragment(new InfoFragment());
+                    return true;
+                } else if (itemId == R.id.bottom_star) {
+
+//                  openFragment(new );
+//                    return true;
+//                    Toast.makeText(this, "Chair", Toast.LENGTH_LONG).show();
+                }
+
+                return false;
+            }
+        });
+        fragmentManager = getSupportFragmentManager();
+        openFragment(new HomeFragment());
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int itemId = menuItem.getItemId();
+        if (itemId == R.id.nav_search) {
+            openFragment(new SearchFragment());
+        } else if (itemId == R.id.nav_allContact) {
+            openFragment(new AllContactFragment());
+        } else if (itemId == R.id.nav_chair) {
+//            openFragment(new );
+            Toast.makeText(this, "Chair", Toast.LENGTH_LONG).show();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void openFragment(Fragment fragment){
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.commit();
     }
 }
